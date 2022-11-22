570f7f53d81a066def00d8e6f76d22655b69470c
Fede-Arevalo <arevalo.feder@gmail.com>
2022-11-22 22:55:09 +0100

    contador básico

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
-const Counter = () => {
+const Counter = (props) => {
+  const [count, setCount] = useState(props.value);
+ 
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+  
   return (
-    <div>Counter</div>
-  )
-}
+    <div>
+      <div>Counter: {count}</div>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+};
 
-export default Counter
+export default Counter;
